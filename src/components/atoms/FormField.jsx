1ad081ad97082e3a723cc12d3d4ea3da83c47ad2
Damon Kreft <damon.kreft@operationorange.uk>
2017-11-29 13:53:05 +0000

    Complete form

--- src/components/atoms/FormField.jsx
+++ src/components/atoms/FormField.jsx
@@ -5,9 +5,10 @@ const TextInputField = ({
   children,
   input,
   label,
+  qaId,
   meta: { touched, error, warning }
 }) => {
-  const clonedChild = React.cloneElement(children, { id: input.name, ...input });
+  const clonedChild = React.cloneElement(children, { id: input.name, 'data-qa-id': qaId, ...input });
 
   return (
     <div>
@@ -15,8 +16,8 @@ const TextInputField = ({
       <div>
         {clonedChild}
         {touched &&
-          ((error && <span>{error}</span>) ||
-            (warning && <span>{warning}</span>))}
+          ((error && <div>{error}</div>) ||
+            (warning && <div>{warning}</div>))}
       </div>
     </div>
   );
@@ -24,8 +25,20 @@ const TextInputField = ({
 
 TextInputField.propTypes = {
   children: PropTypes.element.isRequired,
-  input: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  input: PropTypes.shape({
+    name: PropTypes.string,
+    onBlur: PropTypes.func,
+    onChange: PropTypes.func,
+    onDragStart: PropTypes.func,
+    onDrop: PropTypes.func,
+    onFocus: PropTypes.func,
+    value: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number
+    ])
+  }),
   label: PropTypes.string,
+  qaId: PropTypes.string,
   meta: PropTypes.shape({
     touched: PropTypes.bool,
     error: PropTypes.string,
@@ -34,7 +47,17 @@ TextInputField.propTypes = {
 };
 
 TextInputField.defaultProps = {
+  input: {
+    name: null,
+    onBlur: null,
+    onChange: null,
+    onDragStart: null,
+    onDrop: null,
+    onFocus: null,
+    value: null
+  },
   label: '',
+  qaId: null,
   meta: {
     touched: false,
     error: '',
--- src/containers/Contact/ContactForm.jsx
+++ src/containers/Contact/ContactForm.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Field, reduxForm } from 'redux-form';
+import { required, email } from 'redux-form-validators';
 import InputField from '../../components/atoms/InputField';
+import Button from '../../components/atoms/Button';
 
 export const ContactForm = ({ handleSubmit }) => (
-  <form onSubmit={handleSubmit}>
-    <Field label="First Name" name="firstName" component={InputField} type="text" />
-    <Field label="Last Name" name="lastName" component={InputField} type="text" />
-    <Field label="Email" name="email" component={InputField} type="email" />
-    <button type="submit">Submit</button>
+  <form onSubmit={handleSubmit} noValidate>
+    <Field
+      label="First Name"
+      name="firstName"
+      component={InputField}
+      type="text"
+      validate={[required({ msg: 'Please provide your first name' })]}
+      qaId="first-name-field"
+    />
+    <Field
+      label="Last Name"
+      name="lastName"
+      component={InputField}
+      type="text"
+      validate={[required({ msg: 'Please provide your last name' })]}
+      qaId="last-name-field"
+    />
+    <Field
+      label="Email"
+      name="email"
+      component={InputField}
+      type="email"
+      validate={[required({ msg: 'Please provide your email' }), email({ msg: 'Invalid email' })]}
+      qaId="email-field"
+    />
+    <Button type="submit">Submit</Button>
   </form>
 );
 
