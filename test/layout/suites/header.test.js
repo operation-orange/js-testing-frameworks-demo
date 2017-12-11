3764af7f71001dc5df0a304a07422cee4ea08703
Damon Kreft <damon.kreft@operationorange.uk>
2017-12-11 12:08:16 +0000

    Page object refactor

--- test/layout/suites/header.test.js
+++ test/layout/suites/header.test.js
@@ -1,8 +1,18 @@
-load('init.js');
+load('vars.js');
+load('pages/home.js');
 
 forAll(devices, function () {
   test('Home page test - device: ${deviceName}', function (device) {
-    const driver = getDriver('/?brand=topshop', device);
-    check(driver, '/header.gspec', device.tags);
+    const driver = createDriver(domain, device.size, device.browser);
+    const homePage = new HomePage(driver).load();
+
+    checkLayout({
+      driver: driver,
+      spec: specPath + '/header.gspec',
+      tags: device.tags,
+      objects: homePage.getAllLocators()
+    });
+
+    driver.close();
   });
 });
